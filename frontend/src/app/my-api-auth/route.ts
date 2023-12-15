9ae0cb2617899e26ca4c259392d884fa359bc806
トーマス <210441057@ccmailg.meijo-u.ac.jp>
2023-12-15 23:10:34 +0900

    feat:キャッシュの削除

---
+++ frontend/src/app/my-api-auth/route.ts
@@ -0,0 +1,10 @@
+import {NextResponse} from 'next/server';
+
+import {getServerSession} from "next-auth/next"
+import {options} from "@/app/options";
+
+export async function GET() {
+    const session = await getServerSession(options) // セッション情報を取得
+
+    return NextResponse.json({message: "ok"});
+}
