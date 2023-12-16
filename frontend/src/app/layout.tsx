5c45f44c78d880cb341e197d09bf86c9051b24df
トーマス <115143247+thomas0124@users.noreply.github.com>
2023-12-16 13:23:12 +0900

    Update layout.tsx

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -4,6 +4,8 @@ import './globals.css'
 
 const inter = Inter({ subsets: ['latin'] })
 
+export const dynamic = 'force-dynamic'
+
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="ja">
