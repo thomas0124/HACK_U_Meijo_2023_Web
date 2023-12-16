215424d1c08885a3d502fc1e0a95ee9f77d93fd4
トーマス <115143247+thomas0124@users.noreply.github.com>
2023-12-16 12:34:44 +0900

    Update layout.tsx

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -8,7 +8,7 @@ const title = 'ADvertEX'
 const description =
   '探索&発見バトルゲーム。 街中や地元、学校内にある広告を撮影して、自分だけのキャラクターを創り、戦う。'
 const images = [{ url: 'https://hack-u-meijo-2023-web.vercel.app/opengraph-image.png', alt: 'ADvertEX Image' }]
-const url = 'https://hack-u-meijo-2023-web.vercel.app'
+const url = 'https://hack-u-meijo-2023-web.vercel.app/'
 const type = 'website'
 const locale = 'ja_JP'
 
