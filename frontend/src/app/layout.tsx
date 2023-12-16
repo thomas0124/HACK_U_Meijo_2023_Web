06b83f52d3aebe17bd329992368a015fc25fd656
トーマス <210441057@ccmailg.meijo-u.ac.jp>
2023-12-16 11:13:52 +0900

    feat:OGPの設定

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -4,16 +4,39 @@ import './globals.css'
 
 const inter = Inter({ subsets: ['latin'] })
 
+const siteName = 'ADvertEX'
+const description = 'hack u Meijo 2023 repository Web ver'
+const url = 'https://hack-u-meijo-2023-web.vercel.app/'
+
 export const metadata: Metadata = {
-  title: 'ADvertEX',
-  description: 'hack u Meijo 2023 repository Web ver',
+  title: {
+    default: siteName,
+    template: `%s - ${siteName}`
+  },
+  description,
+  openGraph: {
+    title: siteName,
+    description,
+    url,
+    siteName,
+    locale: 'en_EN',
+    type: 'website'
+  },
+  twitter: {
+    card: `summary_large_image`,
+    title: siteName,
+    description,
+    creator: `@Tomas_engineer`
+  },
+  verification: {
+    google: 'https://www.google.com'
+  },
+  alternates: {
+    canonical: url
+  }
 }
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode
-}) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
       <body className={inter.className}>{children}</body>
